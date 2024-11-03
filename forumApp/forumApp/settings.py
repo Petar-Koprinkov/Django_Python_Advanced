07fd51b9e85a57fc549e67d4a8ba984c4594cc15
Petar Koprinkov <petur_koprinkov@abv.bg>
2024-11-03 14:55:22 +0200

    feat: Added custom user model

--- forumApp/forumApp/settings.py
+++ forumApp/forumApp/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap5',
 
-    'forumApp.posts'
+    'forumApp.posts',
+    'forumApp.accounts',
 ]
 
 MIDDLEWARE = [
@@ -136,3 +137,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
+
+
