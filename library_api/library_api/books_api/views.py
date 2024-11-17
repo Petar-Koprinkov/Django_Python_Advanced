2f826c7ecb4becc457ee406665adceaa66bba77c
Petar Koprinkov <petur_koprinkov@abv.bg>
2024-11-17 20:17:18 +0200

    feat: Added put method

--- library_api/library_api/books_api/views.py
+++ library_api/library_api/books_api/views.py
@@ -31,18 +31,10 @@ class BookSetView(APIView):
         serializer = BookSerializer(book)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def put(self, request, pk: int):
+        book = self.get_book(pk)
+        serializer = BookSerializer(book, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
