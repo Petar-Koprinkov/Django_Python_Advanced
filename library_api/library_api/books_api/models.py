4ed20fe4e22de92e0bc7373b1abc90880b873422
Petar Koprinkov <petur_koprinkov@abv.bg>
2024-11-17 19:29:02 +0200

    feat: Added model for books

--- library_api/library_api/books_api/models.py
+++ library_api/library_api/books_api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(
+        max_length=50
+    )
+
+    pages = models.IntegerField()
+
+    description = models.TextField(
+        max_length=1000,
+        default=''
+    )
+
+    author = models.CharField(
+        max_length=30
+    )
+
+
